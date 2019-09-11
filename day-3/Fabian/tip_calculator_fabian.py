508f7d58e54949886b1865301022d1e34c6b4cb5
fabian-pott <fabian.pott@charite.de>
2019-09-11 16:11:51 +0200

    Added two programs and an updated find motifs file

---
+++ day-3/Fabian/tip_calculator_fabian.py
@@ -0,0 +1,47 @@
+
+import argparse
+
+parser = argparse.ArgumentParser(
+    description=('Calculates a 10, 15, or 20% tip.'))
+
+parser.add_argument(
+    '--tip10',
+    help='Calculates 10% tip.')
+
+parser.add_argument(
+    '--tip15',
+    help='Calculates 15% tip.')
+
+parser.add_argument(
+    '--tip20',
+    help='Calculates 20% tip.')
+
+parser.add_argument(
+    '--tipAll',
+    help='Calculates 20% tip.')
+
+args = parser.parse_args()
+
+if args.tip10:
+	tip = int(args.tip10) * 0.1
+	print('The tip is', tip, '€.')
+
+if args.tip15:
+	tip = int(args.tip15) * 0.15
+	print('The tip is', tip, '€.')
+
+if args.tip20:
+	tip = int(args.tip20) * 0.20
+	print('The tip is', tip, '€.')
+
+if args.tipAll:
+	tip10 = int(args.tipAll) * 0.10
+	tip15 = int(args.tipAll) * 0.15
+	tip20 = int(args.tipAll) * 0.20
+	print('A 10% tip would be', tip10, '€. ' 
+
+		'A 15% tip would be', tip15, '€. ' 
+
+		'A 20% tip would be', tip20, '€.')
+
+

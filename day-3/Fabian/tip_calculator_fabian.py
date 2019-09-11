059e9812e66a555da25f5deddee5b7e749ea1941
fabian-pott <fabian.pott@charite.de>
2019-09-11 16:28:05 +0200

    Added features to the tip calculator

--- day-3/Fabian/tip_calculator_fabian.py
+++ day-3/Fabian/tip_calculator_fabian.py
@@ -24,24 +24,24 @@ args = parser.parse_args()
 
 if args.tip10:
 	tip = int(args.tip10) * 0.1
-	print('The tip is', tip, '€.')
+	print('The tip is', tip, '€. Total is', int(args.tip10)+tip, '€.')
 
 if args.tip15:
 	tip = int(args.tip15) * 0.15
-	print('The tip is', tip, '€.')
+	print('The tip is', tip, '€. Total is', int(args.tip15)+tip, '€.')
 
 if args.tip20:
 	tip = int(args.tip20) * 0.20
-	print('The tip is', tip, '€.')
+	print('The tip is', tip, '€. Total is', int(args.tip20)+tip, '€.')
 
 if args.tipAll:
 	tip10 = int(args.tipAll) * 0.10
 	tip15 = int(args.tipAll) * 0.15
 	tip20 = int(args.tipAll) * 0.20
-	print('A 10% tip would be', tip10, '€. ' 
+	print('A 10% tip would be', tip10, '€. Total would be', int(args.tipAll)+tip10, '€.')
 
-		'A 15% tip would be', tip15, '€. ' 
+	print('A 15% tip would be', tip15, '€. Total would be', int(args.tipAll)+tip15, '€.') 
 
-		'A 20% tip would be', tip20, '€.')
+	print('A 20% tip would be', tip20, '€.Total would be', int(args.tipAll)+tip20, '€.')
 
 

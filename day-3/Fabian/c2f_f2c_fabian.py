7996a5635031e5df9348389034c978e6af4ad7cc
fabian-pott <fabian.pott@charite.de>
2019-09-12 10:20:59 +0200

    changed c2f_f2c types to flaot

--- day-3/Fabian/c2f_f2c_fabian.py
+++ day-3/Fabian/c2f_f2c_fabian.py
@@ -5,19 +5,20 @@ parser = argparse.ArgumentParser(
     description=('Convert Celsius to Fahrenheit and the other way around.'))
 
 parser.add_argument(
-    '--tempC',
+    '--tempC', type=float,
     help='The temperature in Celsius.')
 
 parser.add_argument(
-    '--tempF',
+    '--tempF', type=float,
     help='The temperature in Fahrenheit.')
+# defined types as floats to allow decimal numbers
 
 args = parser.parse_args()
 
 if args.tempC:
-	fahrenheit = (int(args.tempC) * 9/5) + 32
+	fahrenheit = (args.tempC * 9/5) + 32
 	print(str(args.tempC), '°C is', fahrenheit, '°F.')
 
 if args.tempF:
-	celsius = (int(args.tempF)-32) * 5/9
+	celsius = (args.tempF-32) * 5/9
 	print(str(args.tempF), '°F is', celsius, '°C.')

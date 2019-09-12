aa6eb1703cb16577a25e74b928b36662fa51d1d1
JackJack65 <jackson.emanuel@gmail.com>
2019-09-12 18:05:49 +0200

    correctly working hw files

---
+++ day-4/extractor_Jackson.py
@@ -0,0 +1,46 @@
+class SubsequenceExtractor:
+    def __init__(self, offsetsFile):
+        self.offsets = []
+
+        with open(offsetsFile) as fp:
+            for line in fp:
+                origLine = line
+                line = line.strip()
+                if not line or line.startswith('#'):
+                    continue
+
+                try: #branch1 check if the whole line is an integer
+                    start = int(line)
+                except ValueError: #the whole line is not an integer
+                    try: #check if there is a hyphen in the line
+                        start, stop = line.split('-')
+                    except ValueError: #there is no hyphen, the input is unacceptable, close the program
+                        print('Could not parse line %r!' % origLine)
+                        continue
+                    else: #there is a hyphen in the line, split into start and stop variables was successful
+                        try: #check if start is an integer
+                            start = int(start)
+                        except ValueError:
+                            print('Could not parse line %r!' % origLine)
+                            continue
+                        try: #check if stop is an integer
+                            stop = int(stop)
+                        except ValueError:
+                            print('Could not parse line %r!' % origLine)
+                            continue
+                        if start > stop:
+                            print('Invalid window selection, start is larger than stop %r!' % origLine)                            
+                else:
+                    stop = start
+
+                start -= 1
+
+                # Error checking! Less than zero? start < stop?
+
+                # print('%s: %d %d' % (line, start, stop))
+                self.offsets.append((start, stop))
+
+    def extract(self, sequence):
+        for start, stop in self.offsets:
+            subsequence = sequence[start:stop]
+            yield (start, stop, subsequence)

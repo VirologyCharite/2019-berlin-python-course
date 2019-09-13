b64ab608f0a2d4e973ecaa55678dcd39a237289a
terrycojones <terry@jon.es>
2019-09-13 13:29:38 +0200

    Added day 5 code

---
+++ day-5/extract-subsequences.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+
+import argparse
+from Bio import SeqIO
+from urllib.parse import quote
+
+from extractor import SubsequenceExtractor
+
+
+parser = argparse.ArgumentParser(
+    description=('Find motifs in sequences in a FASTA file.'))
+
+parser.add_argument(
+    'filename',
+    help='The alignment file.')
+
+parser.add_argument(
+    '--offsetsFile', required=True,
+    help='The file of offsets to extract.')
+
+args = parser.parse_args()
+
+
+with open(args.offsetsFile) as fp:
+    extractor = SubsequenceExtractor(fp)
+
+
+def printHeader():
+    print('''
+<!DOCTYPE html>
+<html>
+  <head>
+  <title>My fabulous web page.</title>
+  </head>
+  <body>
+''')
+
+
+def printFooter():
+    print('''
+    </div>
+  </body>
+</html>
+''')
+
+    
+def NCBISequenceLinkURL(title, field=None, delim='|'):
+    """
+    Given a sequence title, like
+        "acc|GENBANK|AY516849.1|GENBANK|42768646 Homo sapiens",
+    return the URL of a link to the info page at NCBI.
+
+    @param title: The C{str} sequence title to produce a link URL for.
+    @param field: The C{int} field number to use (as delimited by C{delim})
+        or C{None} if no field splitting should be done.
+    @param delim: The C{str} to split the title on (if C{field} is not
+        C{None}).
+    @return: A C{str} URL.
+    """
+    if field is None:
+        ref = title
+    else:
+        try:
+            ref = title.split(delim)[field]
+        except IndexError:
+            raise IndexError(
+                'Could not extract field %d from sequence title %r' %
+                (field, title))
+    return 'http://www.ncbi.nlm.nih.gov/nuccore/' + quote(ref)
+
+
+def NCBISequenceLink(title, field=None, delim='|'):
+    """
+    Given a sequence title, like
+        "acc|GENBANK|AY516849.1|GENBANK|42768646 Homo sapiens",
+    return an HTML <A> tag displaying a link to the info page at NCBI.
+
+    @param title: The C{str} sequence title to produce a link URL for.
+    @param field: The C{int} field number to use (as delimited by C{delim})
+        or C{None} if no field splitting should be done.
+    @param delim: The C{str} to split the title on (if C{field} is not
+        C{None}).
+    @return: A C{str} HTML <A> tag.
+    """
+    return '<a href="%s" target="_blank">%s</a>' % (
+        NCBISequenceLinkURL(title, field, delim), title)
+
+for record in SeqIO.parse(args.filename, 'fasta'):
+    for start, stop, subsequence in extractor.extract(str(record.seq)):
+        print('start=%d, stop=%d, %s' % (start, stop, subsequence))
+
+
+#printHeader()
+
+#for record in SeqIO.parse(args.filename, 'fasta'):
+    #print('<p>%s</p><ol>' % NCBISequenceLink(record.id))
+    #for start, stop, subsequence in extractor.extract(str(record.seq)):
+        #print('<li>start=%d, stop=%d, %s</li>' % (start, stop, subsequence))
+    #print('</ol>')
+
+#printFooter()
